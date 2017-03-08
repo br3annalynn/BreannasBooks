798fd79526f5ad06b008080cb9ab7e3bebac62f9
Breanna <breannalynn@gmail.com>
2017-03-07 22:49:09 -0500

    more clean up

--- BreannasBooks/Startup.cs
+++ BreannasBooks/Startup.cs
@@ -58,8 +58,6 @@ namespace BreannasBooks
 
             app.UseStaticFiles();
 
-            app.UseIdentity();
-
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
